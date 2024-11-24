c577a7ef24b1173e0860b17786991f8e73e772e8
katariyamansi25 <katariyamansi25@gmail.com>
2024-11-24 20:50:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <p className="read-the-docs">
+        welcome here
+      </p>
+    </>
+  )
+}
+
+export default App
